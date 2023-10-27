29380af84b8c95186281d606cafdeb9bc56e47e1
AustinJ712 <austin.j.suhr@gmail.com>
2023-10-26 21:56:33 -0700

    Not sure

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import './global.css'
 
 export default function Page() {
   return (
-    <section>
+    <section style={{ paddingLeft: '10px' }}>
       <div className="flex items-start mb-4" style={{ flexDirection: 'column' }}>
         <div className="flex items-start mb-4">
          <img className="w-24 h-24 rounded-full mr-2" src={'/images/profile-pic/profilePic.jpg'} alt="Profile Image" />
@@ -23,22 +23,26 @@ export default function Page() {
       </p>
       <p style={{ fontWeight: 500, fontSize: '16px' }}>I currently work as the COO of <a className="link" href="https://commonground.io/app">CommonGround</a>, a marketplace that connects farmers, landowners, and real estate professionals aimed at building the future of land transactions.</p>
       <p style={{ fontWeight: 500, fontSize: '16px' }}>My ambition is to build products that people love by sweating the details.</p>
-      <p style={{ fontWeight: 500, fontSize: '16px' }}>To accomplish this, I am a generalist with a passion for end-to-end problem solving that marries<em className="svelte-ikj1jd"> system and interaction design</em>.</p>
+      <p style={{ fontWeight: 500, fontSize: '16px' }}>
+        To accomplish this, I am a generalist with a passion for end-to-end problem solving that marries
+        <em className="svelte-ikj1jd" style={{ fontSize: '16px', fontFamily: 'newsreaderItalicFont', fontWeight: 600 }}> system and interaction design</em>.
+    </p>
+
       <p style={{ fontWeight: 500, fontSize: '16px' }}>Other interests: 3D technologies like NeRF and Gaussian Splatting, drone photography, exercise, playing basketball, reading The Economist and Medium articles on computer science.</p>
       </div> <div className="leading-snug space-y-4">
       <div style={{ padding: '0px 0' }}> {/* This will give some space around the Notable work section */}
               <p style={{ fontWeight: 500, fontSize: '16px', marginBottom: '0px' }}>Notable work:</p>
             
     </div>
-      <div className="my-4"></div>
+      <div className="my-6"></div>
       <a className="project-pair" style={{ marginBottom: '8px' }} href="/projects#cashrentstimate">
         <div className="text-black font-medium" style={{fontWeight: 700, fontSize: '16px'}}>CashRentstimate algorithm <svg width="18" height="18" stroke="#737373" stroke-width="2" className="lucide-icon lucide lucide-arrow-up-right inline text-neutral-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
         </div>
-          <div><p style={{color:"#404040" , fontWeight: 500, fontSize: '16px'}}>An ML-based tool to estimate the value of farmland in a lease agreement.</p>
+          <div><p style={{ color:"#404040" , fontWeight: 500, fontSize: '16px'}}>An ML-based tool to estimate the value of farmland in a lease agreement.</p>
         <aside style={{ color: '#737373', fontWeight: 400, fontSize: '16px'}}>65k+ estimates sought</aside>
         </div>
       </a>
-      <div className="my-4"></div>
+      <div className="my-6"></div>
       <a className="project-pair" style={{ marginBottom: '8px' }} href="/projects#bore">
         <div className="text-black font-medium" style={{fontWeight: 700, fontSize: '16px'}}>Multiparcel Auction Bidding System<svg width="18" height="18" stroke="#737373" stroke-width="2" className="lucide-icon lucide lucide-arrow-up-right inline text-neutral-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
         </div>
@@ -46,7 +50,7 @@ export default function Page() {
         <aside style={{ color: '#737373', fontWeight: 400, fontSize: '16px'}}>1st in the industry</aside>
         </div>
       </a>
-      <div className="my-4"></div>
+      <div className="my-6"></div>
       <a className="project-pair" style={{ marginBottom: '8px' }} href="/projects#bore">
         <div className="text-black font-medium" style={{fontWeight: 700, fontSize: '16px'}}>Cita Reservations<svg width="18" height="18" stroke="#737373" stroke-width="2" className="lucide-icon lucide lucide-arrow-up-right inline text-neutral-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
         </div>
