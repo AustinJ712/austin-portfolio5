9ebc5eb2e19e0c19cced8dde25fb502445bf2514
AustinJ712 <austin.j.suhr@gmail.com>
2023-11-02 15:14:56 -0700

    Updated projects/page.tsx with image properties

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,7 +10,15 @@ function Projects() {
       date: 'June 2023',
       tags: ['Python', 'SQL', 'Machine Learning'],
       description: 'The CashRentstimate tool estimates the value of a tillable property in a cash rental agreement. CommonGround uses a combination of public and proprietary data to formulate the CashRentstimate including data points like soil health, historic yields, location, irrigation status, and more.',
-      images: ['/images/cashrentstimate-4.png'],
+      images: [
+        {
+          src: '/images/cashrentstimate-4.png',
+          alt: 'Austin Suhr CashRentstimate Tool Visualization',
+          loading: 'eager', // or 'lazy'
+          width: 600, // example width
+          height: 400, // example height
+        },
+      ],
       links: [
         {
           url: 'https://commonground.io/app/cashrentstimate',
@@ -23,7 +31,15 @@ function Projects() {
       date: 'June 2023',
       tags: ['Python', 'UI/UX', 'Database Architecture'],
       description: 'Multi-parcel auctions, also known as multi-unit auctions, are a method used for selling multiple, separate land parcels simultaneously in a single auction event. In this auction format, bidders can bid on individual parcels, combinations of parcels, or the entire collection of parcels as a whole. The goal is to maximize the total revenue generated from the sale while allowing bidders the flexibility to determine the most valuable combinations for themselves. The winning combination of bids is typically determined based on the highest total revenue generated or other predefined objectives. Multi-parcel auctions have gained popularity due to their efficiency and ability to accommodate the diverse needs of both buyers and sellers. <br /><br />These types of auctions are the most complicated auctions but common within the land auction industry. Creating the best multi-parcel auction solution will allow us to provide an offering that is unmatched with other auction providers.',
-      images: ['/images/multiparcel-auction-1.png'],
+      images: [
+        {
+          src: '/images/multiparcel-auction-1.png',
+          alt: 'Austin Suhr Multiparcel Auction Bidding System Visualization',
+          loading: 'lazy', // or 'eager'
+          width: 600, // example width
+          height: 400, // example height
+        },
+      ],
       links: [
         {
           url: 'https://replit.com/@AustinSuhr/MPA8?v=1',
@@ -37,7 +53,15 @@ function Projects() {
       date: 'June 2023',
       tags: ['UI/UX', 'Database Architecture'],
       description: 'Multi-parcel auctions, also known as multi-unit auctions, are a method used for selling multiple, separate land parcels simultaneously in a single auction event. In this auction format, bidders can bid on individual parcels, combinations of parcels, or the entire collection of parcels as a whole. The goal is to maximize the total revenue generated from the sale while allowing bidders the flexibility to determine the most valuable combinations for themselves. The winning combination of bids is typically determined based on the highest total revenue generated or other predefined objectives. Multi-parcel auctions have gained popularity due to their efficiency and ability to accommodate the diverse needs of both buyers and sellers. <br /><br />These types of auctions are the most complicated auctions but common within the land auction industry. Creating the best multi-parcel auction solution will allow us to provide an offering that is unmatched with other auction providers.',
-      images: ['images/multiparcel-auction.png'],
+      images: [
+        {
+          src: '/images/multiparcel-auction-1.png',
+          alt: 'Austin Suhr Multiparcel Auction Bidding System Visualization',
+          loading: 'lazy', // or 'eager'
+          width: 600, // example width
+          height: 400, // example height
+        },
+      ],
       links: [
         {
           url: 'https://demo1.link',
